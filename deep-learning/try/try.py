8fbcb330b58596dd493f3d94c6ce6ff0071df6aa
shuzhiwen <448875211@qq.com>
2018-10-23 14:57:39 +0800

    local

--- deep-learning/try/try.py
+++ deep-learning/try/try.py
@@ -1,47 +1,48 @@
-# 我们会用到NumPy来处理各种训练数据
-import numpy as np
 import tensorflow as tf
 
-# 创建一个特征向量列表，该特征列表里只有一个特征向量，
-# 该特征向量为实数向量，只有一个元素的数组，且该元素名称为 x，
-# 我们还可以创建其他更加复杂的特征列表
-feature_columns = [tf.feature_column.numeric_column("x", shape=[1])]
+# 创建节点时设置name，方便在图中识别
+W = tf.Variable([0], dtype=tf.float32, name='W')
+b = tf.Variable([0], dtype=tf.float32, name='b')
 
-# 创建一个LinearRegressor训练器，并传入特征向量列表
-estimator = tf.estimator.LinearRegressor(feature_columns=feature_columns)
+# 创建节点时设置name，方便在图中识别
+x = tf.placeholder(tf.float32, name='x')
+y = tf.placeholder(tf.float32, name='y')
 
-# 保存训练用的数据
-x_train = np.array([1., 2., 3., 6., 8.])
-y_train = np.array([4.8, 8.5, 10.4, 21.0, 25.3])
+# 线性模型
+linear_model = W * x + b
 
-# 保存评估用的数据
-x_eavl = np.array([2., 5., 7., 9.])
-y_eavl = np.array([7.6, 17.2, 23.6, 28.8])
+# 损失模型隐藏到loss-model模块
+with tf.name_scope("loss-model"):
+    loss = tf.reduce_sum(tf.square(linear_model - y))
+    # 给损失模型的输出添加scalar，用来观察loss的收敛曲线
+    tf.summary.scalar("loss", loss)
 
-# 用训练数据创建一个输入模型，用来进行后面的模型训练
-# 第一个参数用来作为线性回归模型的输入数据
-# 第二个参数用来作为线性回归模型损失模型的输入
-# 第三个参数batch_size表示每批训练数据的个数
-# 第四个参数num_epochs为epoch的次数，将训练集的所有数据都训练一遍为1次epoch
-# 低五个参数shuffle为取训练数据是顺序取还是随机取
-train_input_fn = tf.estimator.inputs.numpy_input_fn(
-    {"x": x_train}, y_train, batch_size=2, num_epochs=None, shuffle=True)
+optmizer = tf.train.GradientDescentOptimizer(0.001)
 
-# 再用训练数据创建一个输入模型，用来进行后面的模型评估
-train_input_fn_2 = tf.estimator.inputs.numpy_input_fn(
-    {"x": x_train}, y_train, batch_size=2, num_epochs=1000, shuffle=False)
+train = optmizer.minimize(loss)
 
-# 用评估数据创建一个输入模型，用来进行后面的模型评估
-eval_input_fn = tf.estimator.inputs.numpy_input_fn(
-    {"x": x_eavl}, y_eavl, batch_size=2, num_epochs=1000, shuffle=False)
+x_train = [1, 2, 3, 6, 8]
+y_train = [4.8, 8.5, 10.4, 21.0, 25.3]
 
-# 使用训练数据训练1000次
-estimator.train(input_fn=train_input_fn, steps=1000)
 
-# 使用原来训练数据评估一下模型，目的是查看训练的结果
-train_metrics = estimator.evaluate(input_fn=train_input_fn_2)
-print("train metrics: %r" % train_metrics)
+sess = tf.Session()
+init = tf.global_variables_initializer()
+sess.run(init)
 
-# 使用评估数据评估一下模型，目的是验证模型的泛化性能
-eval_metrics = estimator.evaluate(input_fn=eval_input_fn)
-print("eval metrics: %s" % eval_metrics)
+# 调用 merge_all() 收集所有的操作数据
+merged = tf.summary.merge_all()
+
+# 模型运行产生的所有数据保存到 /tmp/tensorflow 文件夹供 TensorBoard 使用
+writer = tf.summary.FileWriter('/tmp/tensorflow', sess.graph)
+
+# 训练10000次
+for i in range(10000):
+    # 训练时传入merge
+    summary, _ = sess.run([merged, train], {x: x_train, y: y_train})
+    # 收集每次训练产生的数据
+    writer.add_summary(summary, i)
+
+curr_W, curr_b, curr_loss = sess.run(
+    [W, b, loss], {x: x_train, y: y_train})
+
+print("After train W: %s b %s loss: %s" % (curr_W, curr_b, curr_loss))
